d5b7a10e0eb72bda9d301e0743b080474230994f
喻林 <yulin_yl@126.com>
2020-03-26 19:05:31 +0800

    generate password

--- lib/conduit/accounts/user.ex
+++ lib/conduit/accounts/user.ex
@@ -9,6 +9,8 @@ defmodule Conduit.Accounts.User do
     field :username, :string
     field :password_hash, :string
 
+    field :password, :string, virtual: true
+
     timestamps()
   end
 
@@ -17,7 +19,24 @@ defmodule Conduit.Accounts.User do
     user
     |> cast(attrs, [:email, :username, :bio, :image])
     |> validate_required([:email, :username, :bio, :image])
+    |> validate_length(:username, min: 3, max: 20)
+    # refer: https://gist.github.com/mgamini/4f3a8bc55bdcc96be2c6
+    |> validate_format(:email, ~r/^[\w.!#$%&’*+\-\/=?\^`{|}~]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/i)
     |> unique_constraint(:username)
     |> unique_constraint(:email)
+  end
+
+  def register_chagneset(user, attrs) do
+    user
+    |> changeset(attrs)
+    |> validate_required([:password])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case fetch_change(changeset, :password) do
+      :error -> changeset
+      {:ok, password} -> put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(password))
+    end
   end
 end
