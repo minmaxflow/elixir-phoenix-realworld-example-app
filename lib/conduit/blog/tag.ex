ae6553effe31d9c69a6fcd189933577365f78aaa
喻林 <yulin_yl@126.com>
2020-03-28 17:23:32 +0800

    create tag

---
+++ lib/conduit/blog/tag.ex
@@ -0,0 +1,18 @@
+defmodule Conduit.Blog.Tag do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "tags" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(tag, attrs) do
+    tag
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> unique_constraint(:name)
+  end
+end
